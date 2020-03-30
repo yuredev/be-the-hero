8f2f44628788750ebf41988f14e06c3864294a0c
Yurematias <yurematias@ufrn.edu.br>
2020-03-30 19:10:18 -0300

    updated cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,9 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors({
-    origin,
-}));
 
 // é necessário usar explicitar ao app que receberá jsons no corpo
 
